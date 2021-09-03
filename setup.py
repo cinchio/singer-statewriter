da397656a3d712261fcb7bdcd8b24d90283414bf
Chris London <me@chrislondon.co>
2021-09-03 11:16:59 -0600

    First attempt

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="singer-statewriter",
+    version="0.1.0",
+    description="A simple script to capture Singer.io state messages and write them to a state file.",
+    author="Cinch",
+    url="https://github.com/cinchio/singer-statewriter",
+    python_requires='>=3.6.0',
+    py_modules=["singer_statewriter"],
+    install_requires=[],
+    entry_points="""
+    [console_scripts]
+    singer-statewriter=singer_statewriter:main
+    """,
+    packages=find_packages(include=['singer_statewriter', 'singer_statewriter.*']),
+    #package_data = {
+    #    "singer_statewriter": ["schemas/*.json"]
+    #},
+    #include_package_data=True,
+)
---
+++ singer_statewriter/__init__.py
@@ -0,0 +1,21 @@
+import io
+import json
+import sys
+import argparse
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='A simple script to capture Singer.io state messages and write them to a state file.')
+    parser.add_argument('state', type=str, help='Path to state JSON file')
+    args = parser.parse_args()
+
+    input_messages = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+    for message in input_messages:
+        message = json.loads(message)
+
+        if message['type'] == 'STATE':
+            with open(args.state, 'w') as f:
+                json.dump(message['value'], f, indent=2)
+
+if __name__ == "__main__":
+    main()
